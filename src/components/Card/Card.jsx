31a6e33a56a632f8af422fc63aeaba872a9687de
Sona Babaxanyan <sonul.babaxanyan@gmail.com>
2020-07-18 00:33:39 +0400

    Start

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import question from '../../assets/images/question.png';
 import s from './Card.module.scss';
-import koala from '../../assets/images/koala.png'
+import kittie from '../../assets/images/kittie.png'
 
 const Card = (props) => {
-
-    let flipTheCard = () => {
-        props.flipCard()
-    }
-
     return (<>
 
         <div className={s.item + " " + s.flip} onClick={props.flipCard}>
             {props.flipped ?
-                <img src={koala} alt="" className={s.backFace} />
+                <img src={kittie} alt="" className={s.backFace} />
                 :
                 <img src={question} alt="" className={s.frontFace} />
             }
